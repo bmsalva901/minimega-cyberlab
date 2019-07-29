5b3dbbd24d2eeff049e34dd667ace25d96149810
bmsalva901 <bmsalva90@gmail.com>
2019-07-28 18:30:46 -0600

    fixed file path for kali zip download

--- minimega_install_scripts/.download-vms-03.sh
+++ minimega_install_scripts/.download-vms-03.sh
@@ -8,7 +8,7 @@ sudo gunzip /opt/minimega/images/pfSense-CE-2.4.4-RELEASE-amd64.iso
 echo Downloading Kali 2019.2 from: https://cdimage.kali.org/kali-2019.2/kali-linux-2019.2-amd64.iso
 echo '##########################################################'
 sudo wget https://images.offensive-security.com/virtual-images/kali-linux-2019.2-vmware-amd64.7z -O /opt/minimega/images/kali-linux-2019.2-vmware-amd64.7z
-sudo 7z x kali-linux-2019.2-vmware-amd64.7z
+sudo 7z x /opt/minimega/images/kali-linux-2019.2-vmware-amd64.7z
 sudo qemu-img convert -f vmdk -O qcow2 /opt/minimega/images/Kali-Linux-2019.2-vmware-amd64/Kali-Linux-2019.2-vmware-amd64.vmdk /opt/minimega/images/kali-linux.qcow2
 sudo rm /opt/minimega/images/kali-linux-2019.2-vmware-amd64.7z
 sudo rm -r opt/minimega/images/Kali-Linux-2019.2-vmware-amd64
