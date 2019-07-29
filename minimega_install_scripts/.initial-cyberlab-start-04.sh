13a2e2e96bd90263ad64225b5369ca44acbeb737
bmsalva901 <bmsalva90@gmail.com>
2019-07-28 19:17:46 -0600

    update 7z extraction destination

--- minimega_install_scripts/.initial-cyberlab-start-04.sh
+++
@@ -1,4 +0,0 @@
-sudo cp -r cyberLab/ /opt/minimega/
-echo Starting CyberLab for the first time.
-echo '#######################################################################'
-sudo bash /opt/minimega/cyberLab/startLab
