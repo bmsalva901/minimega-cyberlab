c90c3475bea325698a9381c02aff4c6f136e8ade
bmsalva901 <bmsalva90@gmail.com>
2019-09-04 15:41:37 -0600

    Updated Lab1 script

--- minimega_install_scripts/Lab1/Lab1.mm
+++ minimega_install_scripts/Lab1/Lab1.mm
@@ -3,40 +3,45 @@ sudo /opt/minimega/bin/minimega -attach
 #Simple script to deploy simple setup in minimega
 
 #At the beginning for every script, assign namespace and the following commands
-namespace cyberLab
+namespace Lab1
 optimize ksm true
 ns queueing true
 
-###Configuring individual VMs
-###pfSense firewall with two interfaces (WAN, LAN)
+###Kali
 clear vm config
-disk create qcow2 /opt/minimega/images/pfSense-CE-2.4.4.qcow2 20G
-vm config disk /opt/minimega/images/pfSense-CE-2.4.4.qcow2
-vm config cdrom /opt/minimega/cyberlab/vms/pfSense-CE-2.4.4-RELEASE-amd64.iso 
+vm config disk /opt/minimega/images/Kali-Linux.qcow2
 vm config memory 2048
-vm config net 0 LAN
-vm config snapshot false
+vm config net 0
+vm config snapshot true
 vm config qemu-append -vga qxl
-vm launch kvm pfSense
+vm launch kvm Kali1
 
 ###Kali
 clear vm config
-disk create qcow2 kali-linux-2019.2-amd64.qcow2 20G
-vm config disk /opt/minimega/images/kali-linux.qcow2
-vm config cdrom /opt/minimega/images/iso/kali-linux-2019.2-amd64.iso 
+vm config disk /opt/minimega/images/Kali-Linux.qcow2
 vm config memory 2048
-vm config net LAN
+vm config net 0
 vm config snapshot false
 vm config qemu-append -vga qxl
-vm launch kvm Kali
+vm launch kvm Kali2
 
+###Kali
 clear vm config
-vm config disk /opt/minimega/images/metasploitable.qcow2
-vm config memory 1024
-vm config net LAN
+vm config disk /opt/minimega/images/Kali-Linux.qcow2
+vm config memory 2048
+vm config net 0
 vm config snapshot false
 vm config qemu-append -vga qxl
-vm launch kvm Metasploitable
+vm launch kvm Kali3
+
+###Kali
+clear vm config
+vm config disk /opt/minimega/images/Kali-Linux.qcow2
+vm config memory 2048
+vm config net 0
+vm config snapshot false
+vm config qemu-append -vga qxl
+vm launch kvm Kali4
 
 
 vm launch
