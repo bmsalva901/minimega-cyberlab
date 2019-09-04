509f8af4561f57db9691f919c4917252f2104adf
bmsalva901 <bmsalva90@gmail.com>
2019-09-03 18:58:05 -0600

    changed main config and added Lab1

---
+++ minimega_install_scripts/Lab1/Lab1.mm
@@ -0,0 +1,44 @@
+sudo /opt/minimega/bin/minimega -attach
+
+#Simple script to deploy simple setup in minimega
+
+#At the beginning for every script, assign namespace and the following commands
+namespace cyberLab
+optimize ksm true
+ns queueing true
+
+###Configuring individual VMs
+###pfSense firewall with two interfaces (WAN, LAN)
+clear vm config
+disk create qcow2 /opt/minimega/images/pfSense-CE-2.4.4.qcow2 20G
+vm config disk /opt/minimega/images/pfSense-CE-2.4.4.qcow2
+vm config cdrom /opt/minimega/cyberlab/vms/pfSense-CE-2.4.4-RELEASE-amd64.iso 
+vm config memory 2048
+vm config net 0 LAN
+vm config snapshot false
+vm config qemu-append -vga qxl
+vm launch kvm pfSense
+
+###Kali
+clear vm config
+disk create qcow2 kali-linux-2019.2-amd64.qcow2 20G
+vm config disk /opt/minimega/images/kali-linux.qcow2
+vm config cdrom /opt/minimega/images/iso/kali-linux-2019.2-amd64.iso 
+vm config memory 2048
+vm config net LAN
+vm config snapshot false
+vm config qemu-append -vga qxl
+vm launch kvm Kali
+
+clear vm config
+vm config disk /opt/minimega/images/metasploitable.qcow2
+vm config memory 1024
+vm config net LAN
+vm config snapshot false
+vm config qemu-append -vga qxl
+vm launch kvm Metasploitable
+
+
+vm launch
+vm start all
+
